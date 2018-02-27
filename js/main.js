7ebc3e07f8ab00bd8a6e57358b3e5581d7bc5660
Camden Cheek <ccheek22@gmail.com>
2018-02-26 23:34:26 -0500

    Updated registry page

--- js/main.js
+++ js/main.js
@@ -30,11 +30,11 @@ jQuery(document).ready(function () {
 	
 	
 /*preloder*/
-$(window).load(function() { // makes sure the whole site is loaded
-	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#loader-wrapper').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
-	$('body').delay(200).css({'overflow-x':'hidden'});
-})	
+//$(window).load(function() { // makes sure the whole site is loaded
+	//$('#status').fadeOut(); // will first fade out the loading animation
+	//$('#loader-wrapper').delay(200).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	//$('body').delay(200).css({'overflow-x':'hidden'});
+//})	
 	
 	
 	
